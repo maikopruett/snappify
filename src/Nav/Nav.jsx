dc65bef9d83bc5f751bca493496f5f862736f0e5
Maiko Pruett <maikopruett@gmail.com>
2025-01-03 04:41:59 -0800

    init nav and main page

---
+++ src/Nav/Nav.jsx
@@ -0,0 +1,15 @@
+import React from 'react'
+import './Nav.css'
+
+export default function Nav() {
+  return (
+    <>
+        <div className='nav-container'>
+            <h1 className='logo'>SNAPP</h1>
+        </div>
+        <div className='nav-container-mobile'>
+            <h1 className='logo'>SNAPP</h1>
+        </div>
+    </>
+  )
+}

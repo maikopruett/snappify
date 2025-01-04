ede8fbf27f78dc38f8ab130995da8022fddae392
Maiko Pruett <maikopruett@gmail.com>
2025-01-03 20:24:16 -0800

    Main page header

--- src/pages/Main/Main.jsx
+++ src/pages/Main/Main.jsx
@@ -4,7 +4,7 @@ import './Main.css'
 export default function Main() {
   return (
     <div className='main-container'>
-
+      <h1 id="main-header">Tired of Paying Monthly Hosting Fees? <br /> Ditch the Costs—Go Snapp!</h1>
     </div>
   )
 }

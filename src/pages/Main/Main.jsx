4ad9d4ee664f6cd09334c28c5cdd1117b1efa4c8
Maiko Pruett <maikopruett@gmail.com>
2025-01-03 20:28:30 -0800

    better header

--- src/pages/Main/Main.jsx
+++ src/pages/Main/Main.jsx
@@ -4,7 +4,7 @@ import './Main.css'
 export default function Main() {
   return (
     <div className='main-container'>
-      <h1 id="main-header">Tired of Paying Monthly Hosting Fees? <br /> Ditch the Costs—Go Snapp!</h1>
+      <h1 id="main-header">Forget Monthly Hosting Fees! <br /> Ditch Costs & Go SNAPP!</h1>
     </div>
   )
 }

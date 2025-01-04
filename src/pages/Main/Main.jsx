daee6bcddf29e5c8f9f530771bc23c77a8ddef32
Maiko Pruett <maikopruett@gmail.com>
2025-01-03 20:56:56 -0800

    added call to action btn

--- src/pages/Main/Main.jsx
+++ src/pages/Main/Main.jsx
@@ -5,6 +5,8 @@ export default function Main() {
   return (
     <div className='main-container'>
       <h1 id="main-header">Forget Monthly Hosting Fees! <br /> Ditch Costs & Go SNAPP</h1>
+      <p id='main-subheader'>Snapp builds beautiful websites without the recurring costs—just pay once and own it!</p>
+      <button id='call-to-action-btn'>Get started free</button>
     </div>
   )
 }

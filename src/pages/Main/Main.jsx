2483c69d57a72b912a770e6ee0ab4b981f7f43e5
Maiko Pruett <maikopruett@gmail.com>
2025-01-04 17:39:00 -0800

    added pricing compenent

--- src/pages/Main/Main.jsx
+++ src/pages/Main/Main.jsx
@@ -12,6 +12,24 @@ export default function Main() {
       <h1 id="main-header">Forget Monthly Hosting Fees! <br /> Ditch Costs & Go SNAPP</h1>
       <p id='main-subheader'>Snapp builds beautiful websites without the recurring costs—just pay once and own it!</p>
       <button id='call-to-action-btn'>Get started free</button>
+      
+      <div className='main-pricing-container'>
+        <div className='main-competitor-pricing'>
+          <h1>Competitors Pricing</h1>
+          <h2>Yearly Operating Cost</h2>
+          <h3>Wix<span>$183.6</span></h3>
+          <h3>Squarespace<span>$172.8</span></h3>
+          <h3>Shopify<span>$348</span></h3>
+          <h3>Hostinger<span>$143.88</span></h3>
+          <h3>GoDaddy<span>$143.88</span></h3>
+        </div>
+        <div className='main-pricing'>
+          <h1>SNAPP Pricing</h1>
+          <h2>Yearly Operating Cost</h2>
+          <p>Just a one time initial fee averaging only $60!</p>
+          <h3>SNAPP - $12</h3>
+        </div>
+      </div>
     </div>
   )
 }

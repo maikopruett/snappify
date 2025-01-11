7ee88a6a5c64062c997f8928437caade79c0bc33
Maiko Pruett <maikopruett@gmail.com>
2025-01-11 13:24:05 -0800

    init pricing page

--- src/App.jsx
+++ src/App.jsx
@@ -4,6 +4,7 @@ import Main from './pages/Main/Main'
 import Nav from './Nav/Nav'
 import Contact from './pages/Contact/Contact'
 import Portfolio from './pages/Portfolio/Portfolio'
+import Pricing from './pages/Pricing/Pricing'
 
 function App() {
   return (
@@ -13,6 +14,7 @@ function App() {
         <Route path='/' element={ <Main/> }/>
         <Route path='/contact' element={ <Contact/> }/>
         <Route path='/portfolio' element={ <Portfolio/> }/>
+        <Route path='/pricing' element={ <Pricing/> }/>
       </Routes>
     </BrowserRouter>
   )
